843f3b0dee9df49e3c1180829f40dc5296967657
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2022-03-09 09:29:22 +0900

    ConcatTrigger追加

---
+++ Assets/Scripts/ERBehaviours/Trigger/ConcatTrigger.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace ER.ERBehaviour
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Serializable]
+    public sealed class ConcatTrigger : ITrigger
+    {
+        [SerializeReference, SubclassSelector(typeof(ITrigger))]
+        private List<ITrigger> triggers = default;
+
+        public IObservable<Unit> AsObservable(IBehaviourData data)
+        {
+            return Observable.Defer(() =>
+            {
+                var stream = this.triggers[0].AsObservable(data);
+                for(var i=1; i<this.triggers.Count; i++)
+                {
+                    var index = i;
+                    stream = stream.SelectMany(_ => this.triggers[index].AsObservable(data));
+                }
+
+                return stream;
+            });
+        }
+    }
+}
