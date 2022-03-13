5f62998e4b576642df473d1b18b261194b8f3f58
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2022-03-13 22:00:34 +0900

    とりあえず必要なチャンクを作る

---
+++ Assets/Scripts/StageChunk.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace ER.StageControllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class StageChunk : MonoBehaviour
+    {
+#if UNITY_EDITOR
+        [ContextMenu("Test")]
+        private void Test()
+        {
+            var name = this.name;
+            var startIndex = name.IndexOf("(");
+            if(startIndex == -1 && name.IndexOf("Base") == -1)
+            {
+                Debug.Log($"{name}を正しくパースできません");
+            }
+
+            var endIndex = name.IndexOf(",", startIndex);
+            var x = int.Parse(name.Substring(startIndex + 1, endIndex - startIndex - 1));
+            startIndex = endIndex;
+            endIndex = name.IndexOf(",", startIndex + 1);
+            var y = int.Parse(name.Substring(startIndex + 1, endIndex - startIndex - 1));
+
+            this.transform.localPosition = new Vector3(x * StageLoader.SplitSize * 2, y * StageLoader.SplitSize * 2, 0);
+            UnityEditor.EditorUtility.SetDirty(this);
+        }
+#endif
+    }
+}
