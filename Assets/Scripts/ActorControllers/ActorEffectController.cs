6bddc6117ef7935dc3bd4265d9a314a11af1f958
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2022-03-27 08:40:05 +0900

    ActorEffectController実装中

---
+++ Assets/Scripts/ActorControllers/ActorEffectController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.Playables;
+
+namespace ER.ActorControllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class ActorEffectController : MonoBehaviour
+    {
+        [SerializeField]
+        private PlayableDirector recoveryItemPrefab = default;
+
+
+    }
+}
