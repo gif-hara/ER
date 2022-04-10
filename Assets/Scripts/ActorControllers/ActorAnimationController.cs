72baf3c804e68268f95948ac3b1429979916d287
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2022-04-10 18:09:30 +0900

    ActorAnimationController

---
+++ Assets/Scripts/ActorControllers/ActorAnimationController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace ER.ActorControllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class ActorAnimationController : MonoBehaviour
+    {
+        [SerializeField]
+        private AnimationClip avoidanceClip = default;
+    }
+}
