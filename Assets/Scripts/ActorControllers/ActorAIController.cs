f7cd48dfc68ba967ba2d6fc21a2489582791cb53
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2022-05-06 00:20:07 +0900

    ノックバック時にAIが正しく動作していなかったので修正

--- Assets/Scripts/ActorControllers/ActorAIController.cs
+++ Assets/Scripts/ActorControllers/ActorAIController.cs
@@ -22,6 +22,12 @@ namespace ER.ActorControllers
         [SerializeField]
         private BehaviourHolder initialAI = default;
 
+        /// <summary>
+        /// ノックバックした際に切り替わるAI
+        /// </summary>
+        [SerializeField]
+        private BehaviourHolder onKnockBackedAI = default;
+
         [SerializeReference, SubclassSelector(typeof(IEquipmentSelector))]
         private IEquipmentSelector rightEquipmentSelector = default;
 
@@ -35,6 +41,17 @@ namespace ER.ActorControllers
 
         private void Start()
         {
+            this.actor.Broker.Receive<ActorEvent.OnChangedStateType>()
+                .Buffer(2, 1)
+                .Subscribe(x =>
+                {
+                    if (x[0].NextState == ActorStateController.StateType.KnockBack)
+                    {
+                        this.ChangeRequest(this.onKnockBackedAI);
+                    }
+                })
+                .AddTo(this.actor.Disposables);
+            
             this.ChangeRequest(this.initialAI);
             this.rightEquipmentSelector.Attach(this.actor, 0);
 
