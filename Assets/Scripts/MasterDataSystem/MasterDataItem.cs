203b06db2e214da18236f69ee00dad3557381df3
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2022-05-09 22:34:27 +0900

    貴重品もマスターデータ化

--- Assets/Scripts/MasterDataSystem/MasterDataItem.cs
+++ Assets/Scripts/MasterDataSystem/MasterDataItem.cs
@@ -20,17 +20,29 @@ namespace ER.MasterDataSystem
             // なので参照するマスターデータは先に読み込まれている必要があります
 
             this.raw.Clear();
+            
+            // 武器
             foreach (var i in MasterDataWeapon.Instance.Records)
             {
                 this.raw.Add(i.Id, new Record(i.Id, false, ItemCategory.Weapon));
             }
+            
+            // 盾
             foreach (var i in MasterDataShield.Instance.Records)
             {
                 this.raw.Add(i.Id, new Record(i.Id, false, ItemCategory.Shield));
             }
+            
+            // 防具
             foreach (var i in MasterDataArmor.Instance.Records)
             {
                 this.raw.Add(i.Id, new Record(i.Id, false, i.ArmorType.ToItemCategory()));
+            }
+            
+            // 貴重品
+            foreach (var i in LocalizationManager.GetTermsList("ValuableItem"))
+            {
+                this.raw.Add(i, new Record(i, true, ItemCategory.Valuable));
             }
         }
         [Serializable]
