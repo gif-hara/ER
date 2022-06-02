7e438b9ec541f6b18da1ee727df90f5ee0b2ac42
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2022-06-03 00:01:23 +0900

    Actorが死亡したらイベント実行できるように

--- Assets/Scripts/StageControllers/ActorSpawnerOnSetup.cs
+++ Assets/Scripts/StageControllers/ActorSpawnerOnSetup.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
 using ER.ActorControllers;
+using ER.ERBehaviour;
 using ER.MasterDataSystem;
 using UnityEngine;
 using UnityEngine.Assertions;
+using UniRx;
 
 namespace ER.StageControllers
 {
@@ -13,6 +16,9 @@ namespace ER.StageControllers
         [SerializeField]
         private string actorStatusId = default;
 
+        [SerializeReference, SubclassSelector(typeof(IAction))]
+        private List<IAction> onDeadActions;
+
         public void Setup(StageController stageController)
         {
             if (stageController.GimmickSpawnManager.CanSpawnEnemy(this.transform, out var id))
@@ -20,11 +26,24 @@ namespace ER.StageControllers
                 stageController.GimmickSpawnManager.AddSpawnedEnemy(id);
                 var t = this.transform;
                 var masterDataActorStatus = MasterDataActorStatus.Get(this.actorStatusId);
-                masterDataActorStatus.actorPrefab.Spawn(
+                var actor = masterDataActorStatus.actorPrefab.Spawn(
                     t.position,
                     t.rotation,
                     actorStatusId
                     );
+
+                if (this.onDeadActions.Count > 0)
+                {
+                    actor.Broker.Receive<ActorEvent.OnDead>()
+                        .Subscribe(_ =>
+                        {
+                            var actorHolder = new ActorHolderBehaviourData();
+                            foreach (var onDeadAction in this.onDeadActions)
+                            {
+                                onDeadAction.Invoke(actorHolder);
+                            }
+                        });
+                }
             }
         }
     }
